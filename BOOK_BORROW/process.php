575daa6a607f4774091fd10c444aa2b0c4cff442
MD MAHMUDUR RAHMAN <105537287+Abirrahman2@users.noreply.github.com>
2024-11-26 18:13:32 +0600

    Update process.php

--- BOOK_BORROW/process.php
+++ BOOK_BORROW/process.php
@@ -5,7 +5,7 @@
       $checkstudentname=trim($_POST['student-name']);
       $checkstudentid=trim($_POST['student-id']);
       $checkstudentemail=trim($_POST['student-email']);
-      $booklist= $booklist = isset($_POST['booklist']) ? trim($_POST['booklist']) : '';
+       $booklist = isset($_POST['booklist']) ? trim($_POST['booklist']) : '';
       $errors=[];
     if (!(preg_match("/^[a-zA-Z ]+$/", $checkstudentname))) 
     {
@@ -18,10 +18,17 @@
          $track=true;
          $errors[]="PROVIDED ID IS INVALID";
     }
-    if(!(preg_match("/^\d{2}-\d{5}-\d{1}+\@+(student)+\.+(aiub)+\.+(edu)/",$checkstudentemail)))
+    if(!(preg_match("/^\d{2}-\d{5}-\d{1}+\@+(student)+\.+(aiub)+\.+(edu)$/",$checkstudentemail)))
     {
         $track=true;
         $errors[]="PROVIDED EMAIL IS INVALID";
+    }
+    $emailvarification=explode("@",$checkstudentemail);
+    $emailfirstpart=$emailvarification[0];
+    if($emailfirstpart!=$checkstudentid)
+    {
+        $track=true;
+        $errors[]="STUDENT ID AND EMAIL'S FIRST PART IS NOT SAME".$emailfirstpart;
     }
     if (empty($booklist) || $booklist == "") {
         $track = true;
@@ -69,35 +76,75 @@
             }
          echo "</ul>";
       }
-     else{
-        echo "SUCCESSFULLY SUBMITTED";
-        $all_data = [
-            "student_name" => $checkstudentname,
-            "student_id" => $checkstudentid,
-            "student_email" => $checkstudentemail,
-            "selected_book" => $booklist,
-            "borrow_date" => $checkborrowdate,
-            "return_date" => $checkreturndate
+     else
+     {
+        $book_info = [
+            'student_name'=>$checkstudentname,
+            'student_id' => $checkstudentid,
+            'borrow_date' => $checkborrowdate,
+            'return_date' =>$checkreturndate
         ];
 
-        $json_data = json_encode($all_data);
-
-        $transfer_file = 'save_data.json'; 
-        if (file_put_contents($transfer_file, $json_data))
-         {
+          if(isset($_COOKIE[$booklist]))
+          {
+           
+           $user_info_cookie=json_decode($_COOKIE[$booklist],true);
+           if($user_info_cookie['student_name']!=$checkstudentname)
+           {
+            echo "THE BOOK YOU SELECT CURRENTLY IS NOT AVAILABLE. AND IT WILL BE AVAILABLE AFTER  ";
+            echo $user_info_cookie['return_date'];
+           }
+           else{
+    
             echo "<div style='border: 2px solid green; background-color: rgb(230, 255, 230); padding: 10px; margin-bottom: 10px; text-align: center;'>";
-            echo "<h2 style='color: green; margin: 0;'>SUCCESSFULLY SUBMITTED AND SAVED ALL THE INFORMATION IN save_data.json FILE</h2>";
+            echo "<h2 style='color: green; margin: 0;'>YOU ALREADY BORROWED THIS BOOK</h2>";
             echo "</div>";
+           }
+          // header("Location:../book_borrow/warning.php");
+               
+          }
+          else{
+            //echo "SUCCESSFULLY SUBMITTED";
+           
+            //echo $_POST['booklist'];
+                $all_data = [
+                    "student_name" => $checkstudentname,
+                    "student_id" => $checkstudentid,
+                    "student_email" => $checkstudentemail,
+                    "selected_book" => $booklist,
+                    "borrow_date" => $checkborrowdate,
+                    "return_date" => $checkreturndate
+                ];
+        
+                $json_data = json_encode($all_data);
+        
+                $transfer_file = 'save_data.json'; 
+                if (file_put_contents($transfer_file, $json_data))
+                 {
+                    echo "<div style='border: 2px solid green; background-color: rgb(230, 255, 230); padding: 10px; margin-bottom: 10px; text-align: center;'>";
+                    echo "<h2 style='color: green; margin: 0;'>SUCCESSFULLY SUBMITTED AND SAVED ALL THE INFORMATION IN save_data.json FILE</h2>";
+                    echo "</div>";
 
-
-
-        } 
-        else 
-        {
-            echo "<div style='border: 2px solid orange; background-color: rgb(255, 245, 200); padding: 10px; margin-bottom: 10px; text-align: center;'>";
-            echo "<h2 style='color: orange; margin: 0;'>SUBMITTED BUT FAILED TO SAVE DATA DUE TO SOME TECHNICAL ERROR</h2>";
-            echo "</div>";
-        }
+                    $receipt_generat="STUDENT NAME: $checkstudentname<br>"."ID: $checkstudentid<br>"."EMAIL: $checkstudentemail<br>"."SELECTED BOOK: $booklist<br>"."RETURN DATE: $checkreturndate";
+                    echo $receipt_generat;
+                    
+        
+        
+                } 
+                else 
+                {
+                    echo "<div style='border: 2px solid orange; background-color: rgb(255, 245, 200); padding: 10px; margin-bottom: 10px; text-align: center;'>";
+                    echo "<h2 style='color: orange; margin: 0;'>SUBMITTED BUT FAILED TO SAVE DATA DUE TO SOME TECHNICAL ERROR</h2>";
+                    echo "</div>";
+                }
+    
+    
+    
+                setcookie($booklist,json_encode($book_info),time()+30,"/");
+            
+            
+          }
+       
 
      }
 
